362eae611ee341bc775b1b7b57df8f944b20dbc0
mak025 <mak025@edu.zealand.dk>
2025-03-17 10:53:25 +0100

    Added tasks to repo

---
+++ LightSwitch/LightSwitch/Program.cs
@@ -0,0 +1,49 @@
+﻿namespace LightSwitch
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Lights on = true || Lights off = false
+            Console.BackgroundColor = ConsoleColor.Red;
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.WriteLine("POWER OUTAGE! Turn on the lights on the breakerbox!");
+            string input = "";
+            input = Console.ReadLine();
+            bool lights = false;
+
+            //create do-while loop with if and else if statements (three statements in total) where each either turns on, turns off or exits the loop
+            do
+            {
+                if (input == "breakerON")
+                {
+                    Console.BackgroundColor = ConsoleColor.Green;
+                    Console.ForegroundColor = ConsoleColor.Gray;
+                    Console.WriteLine("Let there be light!... Hopefully the power doesnt go.. (type: breakerOFF)");
+                    input = Console.ReadLine();
+                }
+                if (input == "breakerOFF")
+                {
+                    Console.BackgroundColor = ConsoleColor.Red;
+                    Console.ForegroundColor = ConsoleColor.White;
+                    Console.WriteLine("POWER OUTAGE! Turn on the lights on the breakerbox!");
+                    input = Console.ReadLine();
+                }
+                if (input == "exit")
+                {
+                    lights = true;
+                    Console.BackgroundColor = ConsoleColor.Black;
+                }
+
+                else
+                {
+                    Console.WriteLine("ERROR! CODE 40! Command Unknown");
+                    input = Console.ReadLine();
+                }
+            }
+
+            while (lights == false); //condition here
+            
+        }
+    }
+}
